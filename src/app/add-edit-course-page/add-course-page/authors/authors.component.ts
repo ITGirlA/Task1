8e2887911051319f3a50dd48151b9bd3c065af6d
ITGirl <workalsu@gmail.com>
2019-03-10 04:12:18 +0600

    authors

--- src/app/add-edit-course-page/add-course-page/authors/authors.component.ts
+++ src/app/add-edit-course-page/add-course-page/authors/authors.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthorsService } from './authors.service';
+import { Author } from 'src/app/models/author/author';
 
 @Component({
   selector: 'app-authors',
@@ -10,7 +11,21 @@ export class AuthorsComponent implements OnInit {
 
   constructor( private authorsService: AuthorsService) { }
 
+  selectedAuthor: Author;
+  selectedAuthors: Author[];
+  authors: Author[];
+
   ngOnInit() {
+    this.getAuthors();
+  }
+
+  onSelect(author: Author): void {
+    this.selectedAuthor = author;
+  }
+
+  getAuthors(): void {
+    this.authorsService.getItems()
+        .subscribe(authors => this.authors = authors);
   }
 
 }
